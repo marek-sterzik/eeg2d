6430b44221532e449eb58daf6634152f43287243
Marek Sterzik <marek@milimetr.org>
2020-06-15 13:40:13 +0200

    fixed build process

--- config/webpack.config.cjs
+++ config/webpack.config.cjs
@@ -4,7 +4,7 @@ module.exports = {
   entry: './src/eeg2d.js',
   output: {
     filename: 'eeg2d.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, '../dist'),
   },
   mode: 'production'
 };
